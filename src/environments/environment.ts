8a6c10e5800fdf838062adc27666f84467366afd
maggietsang125 <maggiecat218@gmail.com>
2022-02-04 23:11:33 +0800

    set up firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 export const environment = {
   production: false
@@ -14,3 +19,19 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD1tz-pRJeRU882FEDdMCKsHT4kRpSzxX4",
+  authDomain: "finnotedb.firebaseapp.com",
+  projectId: "finnotedb",
+  storageBucket: "finnotedb.appspot.com",
+  messagingSenderId: "581170754601",
+  appId: "1:581170754601:web:671af0940158a5d282d838",
+  measurementId: "G-MGTS5LVFJ7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
