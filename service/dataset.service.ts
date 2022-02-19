52fed6454895d53e6f63c478cc41c750c401acfb
maggietsang125 <maggiecat218@gmail.com>
2022-02-19 12:14:55 +0800

    add dataset service, just sync all only

---
+++ service/dataset.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../src/app/task/task';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
+import { TaskDialogComponent, TaskDialogResult } from 'src/app/task-dialog/task-dialog.component';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatasetService {
+
+  constructor(private store: AngularFirestore) { }
+
+  todo: Observable<Task[]> | undefined;
+  inProgress: Observable<Task[]> | undefined;
+  done: Observable<Task[]> | undefined;
+
+  syncTask() {
+    this.todo = this.store.collection('todo').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+    this.inProgress = this.store.collection('inProgress').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+    this.done = this.store.collection('done').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+  }
+
+  /* 
+    need to decide if every thing put in service emit/get by html tags
+  */
+
+  public syncTodo(): Observable<Task[]> {
+    return this.todo = this.store.collection('todo').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+  }
+
+  public syncInProgress(): Observable<Task[]> {
+    return this.inProgress = this.store.collection('inProgress').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+  }
+
+  public syncDone(): Observable<Task[]> {
+    const asdf = this.store.collection('done').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+    
+    // // only get 2 everytime, reduce loading time due to large amount of data
+    // const qwer = this.store.collection('done', thing => thing.limit(2));
+
+    // asdf.subscribe(thingList => {
+    //   thingList.forEach(thing => {
+    //     console.log("thing service: " + thing.title + ", " + thing.description);
+    //   })
+    // })
+
+    // console.log("end sync " + new Date().toISOString().slice(0, 19));    
+    return this.done = asdf;
+  }
+
+  public editTask(dialogRef: MatDialogRef<TaskDialogComponent, any>, list: 'done' | 'todo' | 'inProgress', task: Task) {
+
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      if (!result) {
+        return;
+      }
+      if (result.delete) {
+        this.store.collection(list).doc(task.id).delete();
+      } else {
+        this.store.collection(list).doc(task.id).update(task);
+      }
+    });
+
+  }
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { DatasetService } from 'service/dataset.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private dialog: MatDialog, private store: AngularFirestore) { }
+  constructor(private dialog: MatDialog, private store: AngularFirestore, private dataset: DatasetService) { }
 
   // todo = this.store.collection('todo').valueChanges({ idField: 'id' }) as Observable<Task[]>;
   // inProgress = this.store.collection('inProgress').valueChanges({ idField: 'id' }) as Observable<Task[]>;
@@ -22,13 +23,46 @@ export class AppComponent {
   todo: Observable<Task[]> | undefined;
   inProgress: Observable<Task[]> | undefined;
   done: Observable<Task[]> | undefined;
+  
+  // todo: Observable<Task[]> = this.dataset.syncTodo();
+  // inProgress: Observable<Task[]> = this.dataset.syncInProgress();
+  // done: Observable<Task[]> = this.dataset.syncDone();
 
   title = 'FinNote';
 
   syncTask() {
-    this.todo = this.store.collection('todo').valueChanges({ idField: 'id' }) as Observable<Task[]>;
-    this.inProgress = this.store.collection('inProgress').valueChanges({ idField: 'id' }) as Observable<Task[]>;
-    this.done = this.store.collection('done').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+
+    // this.todo = this.store.collection('todo').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+    // this.inProgress = this.store.collection('inProgress').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+    // this.done = this.store.collection('done').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+
+    console.log("start sync " + new Date().toISOString().slice(0, 19));
+    this.todo = this.dataset.syncTodo();
+    this.inProgress = this.dataset.syncInProgress();
+    this.done = this.dataset.syncDone();
+    
+    this.todo.subscribe(thingList => {
+      console.log("sync todo time: " + new Date().toLocaleString());
+      thingList.forEach(thing => {
+        console.log("> todo: " + thing.title + ", " + thing.description);
+      })
+    });
+    
+    this.inProgress.subscribe(thingList => {
+      console.log("sync inProgress time: " + new Date().toLocaleString());
+      thingList.forEach(thing => {
+        console.log("> inProgress: " + thing.title + ", " + thing.description);
+      })
+    });
+
+    this.done.subscribe(thingList => {
+      console.log("sync done time: " + new Date().toLocaleString());
+      thingList.forEach(thing => {
+        console.log("> done: " + thing.title + ", " + thing.description);
+      })
+    });
+    
+
   }
 
   newTask(): void {
@@ -66,6 +100,7 @@ export class AppComponent {
         this.store.collection(list).doc(task.id).update(task);
       }
     });
+    // this.dataset.editTask(dialogRef, list, task);
   }
 
   drop(event: CdkDragDrop<Task[] | null>): void {
