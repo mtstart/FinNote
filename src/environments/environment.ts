b20b194fc868e10f0eb04b4fdc8e4b8046617c15
maggietsang125 <maggiecat218@gmail.com>
2022-03-06 22:27:18 +0800

    use finnote2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,20 +4,31 @@
 import { initializeApp } from "firebase/app";
 
 export const environment = {
+  // firebase: {
+  //   apiKey: "AIzaSyD1tz-pRJeRU882FEDdMCKsHT4kRpSzxX4",
+  //   authDomain: "finnotedb.firebaseapp.com",
+  //   projectId: "finnotedb",
+  //   storageBucket: "finnotedb.appspot.com",
+  //   messagingSenderId: "581170754601",
+  //   appId: "1:581170754601:web:fb581b4bedf0c99b82d838",
+  //   measurementId: "G-0QHKF4ZQQW"
+  // },
   firebase: {
-    apiKey: "AIzaSyD1tz-pRJeRU882FEDdMCKsHT4kRpSzxX4",
-    authDomain: "finnotedb.firebaseapp.com",
-    projectId: "finnotedb",
-    storageBucket: "finnotedb.appspot.com",
-    messagingSenderId: "581170754601",
-    appId: "1:581170754601:web:fb581b4bedf0c99b82d838",
-    measurementId: "G-0QHKF4ZQQW"
+    projectId: 'finnote2-70a8c',
+    appId: '1:811871768518:web:9ab62469434dab2ff32cc7',
+    storageBucket: 'finnote2-70a8c.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCKXh9pjACUKNh4yQtlCj4XKMgKvvC2PqQ',
+    authDomain: 'finnote2-70a8c.firebaseapp.com',
+    messagingSenderId: '811871768518',
   },
   production: false
 };
 
 // Initialize Firebase
 const app = initializeApp(environment.firebase);
+initializeApp(environment.firebase);
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
