cd8d639b1bc0f9bc7158175517b62bc289a84dec
maggietsang125 <maggiecat218@gmail.com>
2022-03-08 23:19:13 +0800

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TaskManagementComponent } from './task-management/task-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/project-management'
+  },
+  {
+    path: 'project-management',
+    component: TaskManagementComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
